e67fa34f1a053b147c8c815035ed384bbbcef51c
Alabere-coder <alabere4real247@gmail.com>
2022-12-17 23:42:18 +0100

    first commit

---
+++ js/server.js
@@ -0,0 +1,30 @@
+
+const textBtns = document.querySelectorAll('.contact__btn');
+
+textBtns.forEach(textBtn => {
+    let text = textBtn.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
+})
+
+
+// TO SHOW HIDE NAV MENU
+
+const menu = document.querySelector('.nav__menu');
+const openBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+openBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    openBtn.style.display = 'none';
+})
+
+
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    openBtn.style.display = 'inline-block';
+}
+
+closeBtn.addEventListener('click', closeNav)
